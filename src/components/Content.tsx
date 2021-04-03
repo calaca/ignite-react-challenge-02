961f35763c7d42c5bab639911a78ac3cafc8ccdc
Lorena Calaça <lorena.calaca.od@gmail.com>
2021-04-03 15:04:10 -0300

    complete challenge

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,24 @@
-export function Content() {
-  // Complete aqui
+import { Header } from './Header';
+import { MovieCard } from './MovieCard';
+
+import { HeaderProps } from './Header';
+import { MovieProps } from '../App';
+
+interface ContentProps extends HeaderProps {
+  movies: MovieProps[];
+}
+
+export function Content({ selectedGenreTitle, movies }: ContentProps) {
+  return(
+    <div className="container">
+      <Header selectedGenreTitle={selectedGenreTitle} />
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
